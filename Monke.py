e3c53d086badc6fc6116a0adc8d238be7a20d007
Henrik Gombos <hgombos@mac-henrik.local>
2021-05-23 14:38:49 -0400

    Initial version

---
+++ Monke.py
@@ -0,0 +1,176 @@
+#mothers day gift
+
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("white")
+
+monkey1 = turtle.Turtle()
+monkey1.shape("arrow")
+monkey1.color("#654321")
+monkey1.width(30)
+monkey1.speed(1000)
+
+monkey2 = turtle.Turtle()
+monkey2.shape("arrow")
+monkey2.color("#D2B48C")
+monkey2.width(15)
+monkey2.speed(1000)
+
+monkey3 = turtle.Turtle()
+monkey3.shape("arrow")
+monkey3.color("#000000")
+monkey3.width(20)
+monkey3.speed(100)
+
+def monke():
+    print("Reject humanity..")
+    print("Return to Monke")
+    monkey1.pendown()
+    monkey1.begin_fill()
+    amt = 60
+    while amt > 0:
+        monkey1.right(6)
+        monkey1.forward(10)
+        amt = amt - 1
+    monkey1.end_fill()
+    monkey1.penup()
+    samt = 60
+#monkey arms + body
+    while samt > 0:
+        monkey1.right(6)
+        monkey1.forward(10)
+        samt = samt - 6
+    monkey1.pendown()
+    amt2 = 60
+    while amt2 > 0:
+        monkey1.left(5)
+        monkey1.forward(8)
+        amt2 = amt2 - 3
+    monkey1.penup()
+    monkey1.home()
+    monkey1.left(180)
+    monkey1.pendown()
+    samt2 = 60
+    while samt2 > 0:
+        monkey1.left(6)
+        monkey1.forward(10)
+        samt2 = samt2 - 6
+    amt3 = 60
+    while amt3 > 0:
+        monkey1.right(5)
+        monkey1.forward(8)
+        amt3 = amt3 - 3
+    monkey1.penup()
+    monkey1.home()
+    monkey1.forward(30)
+    monkey1.left(90)
+    monkey1.forward(70)
+    monkey1.pendown()
+    monkey1.forward(15)
+    monkey1.begin_fill()
+    amt4 = 20
+    while amt4 > 0:
+        monkey1.left(9)
+        monkey1.forward(5)
+        amt4 = amt4 - 1
+    monkey1.forward(15)
+    monkey1.left(90)
+    monkey1.forward(60)
+    monkey1.end_fill()
+    monkey1.penup()
+    monkey1.right(90)
+    monkey1.forward(250)
+    monkey1.left(45)
+    monkey1.pendown()
+    amt5 = 10
+    while amt5 > 0:
+        monkey1.right(5)
+        monkey1.forward(15)
+        amt5 = amt5 - 1
+    monkey1.penup()
+    monkey1.left(180)
+    amt6 = 10
+    while amt6 > 0:
+        monkey1.forward(15)
+        monkey1.left(5)
+        amt6 = amt6 - 1
+    monkey1.left(60)
+    monkey1.forward(60)
+    monkey1.pendown()
+    monkey1.forward(10)
+    monkey1.left(30)
+    amt7 = 10
+    while amt7 > 0:
+        monkey1.left(5)
+        monkey1.forward(15)
+        amt7 = amt7 - 1
+    monkey1.penup()
+    monkey2.penup()
+    monkey2.left(90)
+    monkey2.forward(5)
+    monkey2.right(90)
+    monkey2.pendown()
+    monkey2.begin_fill()
+    monkey2.forward(40)
+    amt8 = 30
+    while amt8 > 0:
+        monkey2.left(6)
+        monkey2.forward(2)
+        amt8 = amt8 - 1
+    monkey2.forward(80)
+    amt9 = 30
+    while amt9 > 0:
+        monkey2.left(6)
+        monkey2.forward(2)
+        amt9 = amt9 - 1
+    monkey2.forward(60)
+    monkey2.end_fill()
+    monkey2.penup()
+    monkey2.back(20)
+    monkey2.right(90)
+    monkey2.forward(20)
+    monkey2.left(90)
+    monkey2.begin_fill()
+    monkey2.pendown()
+    amt10 = 60
+    while amt10 > 0:
+        monkey2.right(6)
+        monkey2.forward(8)
+        amt10 = amt10 - 1
+    monkey2.end_fill()
+    monkey2.penup()
+    monkey2.forward(1000)
+    monkey3.penup()
+    monkey3.forward(30)
+    monkey3.left(90)
+    monkey3.forward(20)
+    monkey3.pendown()
+    monkey3.forward(1)
+    monkey3.penup()
+    monkey3.left(90)
+    monkey3.forward(60)
+    monkey3.left(90)
+    monkey3.pendown()
+    monkey3.forward(1)
+    monkey3.penup()
+    monkey3.right(180)
+    monkey3.forward(70)
+    monkey3.right(90)
+    monkey3.forward(5)
+    monkey3.right(90)
+    monkey3.pendown()
+    monkey3.forward(3)
+    monkey3.penup()
+    monkey3.left(90)
+    monkey3.forward(40)
+    monkey3.left(90)
+    monkey3.pendown()
+    monkey3.forward(3)
+    monkey3.penup()
+    monkey3.forward(1000)
+
+    
+        
+    
+    
