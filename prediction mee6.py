9c626d793c2cd9cf345a157c687d8ea2f8ace206
Henrik Gombos <henrik99999@gmail.com>
2022-01-01 18:32:39 -0500

    predicts amount of levels someone gets

---
+++ prediction mee6.py
@@ -0,0 +1,13 @@
+import math
+
+
+
+
+print("mee6 level prediction")
+
+
+def predict(d, t, a):
+    prediction = t + (a * d)
+    print("In " + str(d) + " days this person will have approximately " + str(prediction) + "xp!")
+
+#riceman a, 3800
